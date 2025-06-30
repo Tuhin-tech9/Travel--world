44efa650425adfb01870db9b9a10a8a4305286a6
tuhin <tuhin0608@gmail.com>
2025-07-01 01:31:29 +0530

    Add search box component

--- Component/Helper/searchbox.tsx
+++ Component/Helper/searchbox.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import { FaCalendar, FaMap } from 'react-icons/fa'
 import { FaUserGroup } from 'react-icons/fa6'
 
-type Props = {}
 
-const Searchbox = (props: Props) => {
+
+const Searchbox = () => {
   return (
     <div className='bg-white rounded-lg p-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 items-center justify-center gap-8 mt-4 sm:mt-12 w-[95%] sm:w-[80%]'>
         <div className='flex items-center space-x-6'>
